1c2606c602410be06bde5547448e8c507b9f2d32
Toto Blunt <totoblunt15@gmial.com>
2025-05-24 22:23:35 -0500

    Agregando emoticones

--- frontEnd/interfaz.py
+++ frontEnd/interfaz.py
@@ -7,8 +7,8 @@ def welcome():
     """
     Muestra un mensaje de bienvenida al usuario.
     """
-    st.title("Bienvenido a la aplicación de gestión financiera")
-    st.write("Esta aplicación te ayudará a gestionar tus finanzas personales de manera eficiente.")
+    st.title("Bienvenido a la aplicación de gestión financiera 💵💵")
+    st.write("Esta aplicación te ayudará a gestionar tus finanzas personales de manera eficiente. 💰")
     st.write("Por favor, inicia sesión para continuar.")
     
 #Funcion para mostrar el formulario de inicio de sesión
@@ -16,16 +16,16 @@ def login_form():
     """
     Muestra un formulario de inicio de sesión.
     """
-    st.title("Iniciar sesión")
+    st.title("Iniciar sesión 😊 ")
     username = st.text_input("Nombre de usuario").lower()
     password = st.text_input("Contraseña", type="password")
     
     if st.button("Iniciar sesión"):
         # Aquí iría la lógica para verificar las credenciales del usuario
         if username == 'jose' and password == '1234' or username == 'mily' and password == '1234':
-            st.success("Inicio de sesión exitoso")
+            st.success("Inicio de sesión exitoso 😃😃")
             return username
         else:
-            st.error("Nombre de usuario o contraseña incorrectos")
+            st.error("Nombre de usuario o contraseña incorrectos 😞😞")
     else:
         st.warning("Por favor, ingresa tus credenciales")
