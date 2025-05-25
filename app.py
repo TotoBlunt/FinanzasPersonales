8eecb5953fb51d19f0b6c09f3b80cf25b9234325
Toto Blunt <totoblunt15@gmial.com>
2025-05-24 22:39:14 -0500

    probando

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from frontEnd.interfaz import welcome, login_form
-
-
+import streamlit as st
+# Inicializar lista de transacciones en session_state
+if 'transacciones' not in st.session_state:
+    st.session_state.transacciones = []
+    
 welcome()
 login_form()
