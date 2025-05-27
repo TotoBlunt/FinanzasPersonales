e5ed2193566528f4244d9391835e09b586107803
Toto Blunt <totoblunt15@gmial.com>
2025-05-26 21:50:24 -0500

    peobando secuencia main

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from frontEnd.interfaz import welcome,inicio_sesion, registrar
+from frontEnd.interfaz import welcome, inicio_sesion, registrar
 import streamlit as st
 from logica.gastosingresos import main  
 
+# Inicializa página si no está
 if 'page' not in st.session_state:
     st.session_state['page'] = 'inicio'
 
-if 'username' in st.session_state:
-    st.session_state['page'] = 'app'  # Página principal de la app
+# Redirección si ya hay sesión activa
+if 'username' in st.session_state and st.session_state['page'] != 'app':
+    st.session_state['page'] = 'app'
 
+# Control de navegación
 if st.session_state['page'] == 'app':
     main()
 elif st.session_state['page'] == 'inicio':
     welcome()
 elif st.session_state['page'] == 'login':
     inicio_sesion()
-    main()
 elif st.session_state['page'] == 'registro':
     registrar()
