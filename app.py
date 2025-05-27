c6b85df110a6e6575abf442e7604f785b3a27c29
Toto Blunt <totoblunt15@gmial.com>
2025-05-26 22:18:25 -0500

    probar

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from frontEnd.interfaz import welcome, inicio_sesion, registrar
 import streamlit as st
+from frontEnd.interfaz import welcome, inicio_sesion, registrar
 from logica.gastosingresos import main
 
 if 'page' not in st.session_state:
     st.session_state['page'] = 'inicio'
 
-# Si ya hay sesión iniciada, mostrar la app
-if 'username' in st.session_state and st.session_state['page'] != 'app':
-    st.session_state['page'] = 'app'
-
-# Controlador de navegación
-if st.session_state['page'] == 'app':
+if 'username' in st.session_state:
+    # Usuario logueado, mostrar app principal
     main()
-elif st.session_state['page'] == 'inicio':
-    welcome()
-elif st.session_state['page'] == 'login':
-    inicio_sesion()
-elif st.session_state['page'] == 'registro':
-    registrar()
+else:
+    # Usuario no logueado, mostrar pantallas de bienvenida, login o registro
+    if st.session_state['page'] == 'inicio':
+        welcome()
+    elif st.session_state['page'] == 'login':
+        inicio_sesion()
+    elif st.session_state['page'] == 'registro':
+        registrar()
+
