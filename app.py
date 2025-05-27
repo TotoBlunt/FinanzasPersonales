9d534eee35e541a1a43b628d9b0415b16cfdd422
Toto Blunt <totoblunt15@gmial.com>
2025-05-26 22:13:48 -0500

    add app

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from frontEnd.interfaz import welcome, inicio_sesion, registrar
 import streamlit as st
-from logica.gastosingresos import main  
+from logica.gastosingresos import main
 
-if 'username' in st.session_state:
+if 'page' not in st.session_state:
+    st.session_state['page'] = 'inicio'
+
+# Si ya hay sesión iniciada, mostrar la app
+if 'username' in st.session_state and st.session_state['page'] != 'app':
+    st.session_state['page'] = 'app'
+
+# Controlador de navegación
+if st.session_state['page'] == 'app':
     main()
-else:
-    if st.session_state['page'] == 'inicio':
-        welcome()
-    elif st.session_state['page'] == 'login':
-        inicio_sesion()
-    elif st.session_state['page'] == 'registro':
-        registrar()
-
+elif st.session_state['page'] == 'inicio':
+    welcome()
+elif st.session_state['page'] == 'login':
+    inicio_sesion()
+elif st.session_state['page'] == 'registro':
+    registrar()
