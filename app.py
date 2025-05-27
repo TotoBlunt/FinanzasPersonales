43e440111d22e4a95ae07e5e7850a423771f971a
Toto Blunt <totoblunt15@gmial.com>
2025-05-26 22:09:01 -0500

    probando sin params

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from frontEnd.interfaz import welcome, inicio_sesion, registrar
 import streamlit as st
 from logica.gastosingresos import main  
 
-# Inicializa página si no está
-if 'page' not in st.session_state:
-    st.session_state['page'] = 'inicio'
-
-# Redirección si ya hay sesión activa
-if 'username' in st.session_state and st.session_state['page'] != 'app':
-    st.session_state['page'] = 'app'
-
-# Control de navegación
-if st.session_state['page'] == 'app':
+if 'username' in st.session_state:
     main()
-elif st.session_state['page'] == 'inicio':
-    welcome()
-elif st.session_state['page'] == 'login':
-    inicio_sesion()
-elif st.session_state['page'] == 'registro':
-    registrar()
+else:
+    if st.session_state['page'] == 'inicio':
+        welcome()
+    elif st.session_state['page'] == 'login':
+        inicio_sesion()
+    elif st.session_state['page'] == 'registro':
+        registrar()
+
